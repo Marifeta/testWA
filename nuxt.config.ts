60f1afe24bb284183382df4e0b1ec2f9e7bd22e9
Дарья Гром <d.grom@sprinthost.ru>
2025-12-30 10:37:47 +0300

    sfdf

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,4 +3,10 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   ssr: true,
+  nitro: {
+    prerender: {
+      routes: ['/', '/page1', '/page2'],
+      crawlLinks: true
+    }
+  }
 })
